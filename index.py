598b5e574becb186ec5040dbb648bb046dc8989c
Alaiya <rm.rump@gmail.com>
2023-11-30 16:12:22 +0100

    Update index.py
    Reads JSON, add imports

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
 #!/bin/python
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "TODO: list of accessible routes"
+    
+@app.get("/")
+def get_poi():
+    f = open("/templateData/points.json")
+    data = json.load(f)
+    jsonify(data)
+    return "GET JSON"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
